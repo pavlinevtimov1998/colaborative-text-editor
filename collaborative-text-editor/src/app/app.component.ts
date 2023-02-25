c7a6962d570cc45995beec291a8f5748cd603404
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-25 12:26:40 +0200

    setup a simple text editor

--- collaborative-text-editor/src/app/app.component.ts
+++ collaborative-text-editor/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { Schema, DOMParser, MarkSpec } from 'prosemirror-model';
+import { schema } from 'prosemirror-schema-basic';
+//@ts-ignore
+import { addListNodes } from 'prosemirror-schema-list';
+import { exampleSetup } from 'prosemirror-example-setup';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'collaborative-text-editor';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('editor', { static: true }) editorElementRef!: ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const mySchema = new Schema({
+      nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
+      marks: schema.spec.marks,
+    });
+
+    new EditorView(this.editorElementRef.nativeElement, {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema).parse(
+          this.editorElementRef.nativeElement
+        ),
+        plugins: [...exampleSetup({ schema: mySchema })],
+      }),
+    });
+  }
 }
